f7409815d0b38cd429fb17b07de3a688f2685524
Kykim0818 <Kykim@seegene.com>
2021-09-14 21:29:45 +0900

    init settings todoList

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Study TODO List </h1>
+      <li> 텍스트 적어서 + 버튼 클릭시 목록에 추가</li>
+      <li> - 버튼만 클릭시 목록에서 삭제</li>
+      <li> 체크박스 포함 이름 클릭시 스타일 변화 주기</li>
+
+      <h3> 추가 작업</h3>
+      <li> 선택된 작업에 대해 위 / 아래 버튼을 통해 목록의 순서 조정하기 </li>
     </div>
   );
 }
