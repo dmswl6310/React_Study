165a717d421b38e6d5c3fa1c61e0b47e303b715d
eunji <dmswl6310@naver.com>
2021-09-27 00:40:35 +0900

    feature(todoList) : 투두리스트 만들기 1. 추가버튼 2. 삭제버튼

---
+++ src/Todo_List/ListElement.tsx
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+
+const ListElement = (props: {
+  curTodos: {
+    data: string;
+    checked: boolean;
+  }[];
+  handleTodos: React.Dispatch<
+    React.SetStateAction<
+      {
+        data: string;
+        checked: boolean;
+      }[]
+    >
+  >;
+}) => {
+  const itemOnClick = (idx: number) => {
+    const temp = props.curTodos.filter((value, index) => index !== idx);
+    props.handleTodos(temp);
+  };
+  const handleCheckbox = (idx: number) => {
+    const temp = props.curTodos.filter((value, index) =>
+      index === idx ? { ...value, checked: !value.checked } : value
+    );
+    console.log(temp);
+    props.handleTodos(temp);
+  };
+
+  const liItems = props.curTodos.map((item, idx) => (
+    <li key={idx}>
+      <input
+        type="checkbox"
+        checked={props.curTodos[idx].checked}
+        onChange={() => handleCheckbox(idx)}
+      />
+      {item.data}
+      <button onClick={() => itemOnClick(idx)}>-</button>
+    </li>
+  ));
+  return <ul>{liItems}</ul>;
+};
+export default ListElement;
---
+++ src/Todo_List/TodoList.tsx
@@ -0,0 +1,35 @@
+import { useState, useRef } from "react";
+import ListElement from "./ListElement";
+
+const TodoList = () => {
+  const [todos, setTodos] = useState([
+    {
+      data: "일어나기",
+      checked: false,
+    },
+  ]);
+  const inputRef = useRef<any>(null);
+  const handleClick = () => {
+    setTodos([...todos, { data: inputRef.current.value, checked: false }]);
+    inputRef.current.value = null;
+  };
+  const onKeyPress = (e: any) => {
+    if (e.key === "Enter") {
+      setTodos([...todos, { data: e.target.value, checked: false }]);
+      e.target.value = null;
+    }
+  };
+  return (
+    <>
+      <ListElement curTodos={todos} handleTodos={setTodos} />
+      <input
+        type="text"
+        ref={inputRef}
+        onKeyPress={onKeyPress}
+        placeholder="할일 입력"
+      ></input>
+      <button onClick={handleClick}>입력</button>
+    </>
+  );
+};
+export default TodoList;
