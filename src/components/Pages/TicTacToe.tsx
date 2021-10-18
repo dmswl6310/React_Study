881cd813ed6d9bd0d943690e0b39a4f3ea6a43c5
KiYoung <kykim818@naver.com>
2021-10-19 07:13:30 +0900

    study s2 -2

--- src/components/Pages/TicTacToe.tsx
+++ src/components/Pages/TicTacToe.tsx
@@ -1,36 +1,59 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+import { RootState } from "../../stores";
 import Board from "../Organism/Board";
 import Normal from "../Template/Normal";
 
 const TicTacToe = () => {
-    return (
-        <>
-            <Normal header={<Header />} body={<Body />} footer={<Footer />} />
-        </>
-    )
-}
-
+  const [isGameFinish, setIsGameFinish] = useState<boolean>(false);
+  return (
+    <>
+      <Normal
+        header={<Header />}
+        body={
+          <Body isGameFinish={isGameFinish} setIsGameFinish={setIsGameFinish} />
+        }
+        footer={<Footer isGameFinish={isGameFinish} />}
+      />
+    </>
+  );
+};
 
 const Header = () => {
-    return (
-        <>
-            <h1>React Tic Tac Toe - With Hooks</h1>
-        </>
-    )
-}
+  return (
+    <>
+      <h1>React Tic Tac Toe - With Hooks</h1>
+    </>
+  );
+};
 
-const Body = () => {
-    return (
-        <>
-            <Board />
-        </>
-    )
-}
+const Body = (props: {
+  isGameFinish: boolean;
+  setIsGameFinish: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  return (
+    <>
+      <Board {...props} />
+    </>
+  );
+};
 
-const Footer = () => {
-    return (
-        <>
-            <p>Footer</p>
-        </>
-    )
-}
+const Footer = (props: { isGameFinish: boolean }) => {
+  const userState = useSelector((state: RootState) => state.user.userState);
+
+  return (
+    <Wrapper>
+      <div>History</div>
+      <div>Next Player : {userState === true ? "O" : "X"}</div>
+    </Wrapper>
+  );
+};
 export default TicTacToe;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 322px;
+  margin-top: 20px;
+`;
